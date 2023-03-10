2adb737b1406e89d0ee5135349911c14c1d6a7e9
ShadowHunter22 <kennertobiashartman@gmail.com>
2023-03-10 14:39:40 -0700

    Initial project setup w/ board

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BoardGame msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BoardGame from './components/BoardGame.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BoardGame
   }
 }
 </script>
---
+++ src/components/BoardGame.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="board">
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+    <div class="cell"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BoardGame',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.board {
+  padding-top: 5px;
+  aspect-ratio: 1 / 1;
+  width: 45%;
+  height: 45%;
+  
+  display: flex;                       /* establish flex container */
+  flex-wrap: wrap;                     /* enable flex items to wrap */
+  justify-content: space-around;
+  border: 3px solid;
+  margin-left: auto;
+  margin-right: auto;
+}
+.cell {
+  flex: 0 0 32%;                       /* don't grow, don't shrink, width */
+  margin-bottom: 5px;
+  background-color: #999;
+}
+
+.cell:hover {
+  opacity: 0.5;
+}
+</style>
