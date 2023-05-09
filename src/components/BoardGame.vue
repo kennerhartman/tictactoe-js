8530172049587e62a6aee6d5b9348f2c5d26f857
ShadowHunter22 <kennertobiashartman@gmail.com>
2023-05-09 12:08:27 -0600

    Added license header

--- src/components/BoardGame.vue
+++ src/components/BoardGame.vue
@@ -1,3 +1,10 @@
+<!--
+//
+// Copyright (c) Kenner Hartman. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for details.
+//
+-->
+
 <template>
     <div>
         <div class="title">
